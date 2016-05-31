f1d02dd464f96f8f86572d5e34006253c25908c7
Daniel Martí <mvdan@mvdan.cc>
2016-05-31 15:09:59 +0100

    canonical: extend to cover most features

--- testdata/canonical.sh
+++ testdata/canonical.sh
@@ -11,7 +11,49 @@ foo() {
 	bar
 }
 
+if foo; then bar; fi
+if foo; then
+	bar
+fi
+
+if a; then A; elif b; then B; else C; fi
+if a; then
+	A
+elif b; then
+	B
+else
+	C
+fi
+
+while foo; do bar; done
+while foo; do
+	bar
+done
+
+for foo in a b c; do bar; done
+for foo in a b c; do
+	bar
+done
+
+$foo ${foo} ${#foo} ${foo:-bar}
+
+foo | bar
+foo && bar
+
 (foo)
 (
 	foo
 )
+
+$(foo bar)
+`foo bar`
+
+foo 2>&1
+foo <<EOF
+bar
+EOF
+foo <<<"bar"
+foo <(bar)
+
+$((3 + 4))
+let a=1+2 b=(3 + 4)
