mvdan/sh

commit ca2a636bf05d3954ca2e92e2ebaa286a733a46ab

parse: only remove stopNewline at end of line
Reported in the issue below. Found elsewhere by testing all print cases
with newlines at each end too.

The fix makes sense; the newline may be after some more arguments, so we
can't just set stopNewline to false immediately.

Fixes #9.

parse.go
@@ -216,6 +216,7 @@ func (p *parser) next() {
 			return
 		}
 		if p.stopNewline && b == '\n' {
+			p.stopNewline = false
 			p.advanceTok(STOPPED)
 			return
 		}
@@ -542,6 +543,10 @@ func (p *parser) stmts(stops ...Token) (sts []Stmt) {
 		gotEnd := p.newLine || p.ltok == AND || p.ltok == SEMICOLON
 		if len(sts) > 0 && !gotEnd {
 			p.curErr("statements must be separated by &, ; or a newline")
+		}
+		p.got(STOPPED)
+		if p.eof() {
+			break
 		}
 		var s Stmt
 		if !p.gotStmt(&s, stops...) {
@@ -944,7 +949,6 @@ func (p *parser) gotRedirect() bool {
 	case SHL, DHEREDOC:
 		p.stopNewline = true
 		r.Word = p.followWord(r.Op)
-		p.stopNewline = false
 		p.heredocs = append(p.heredocs, hdocRef{
 			stmt:  s,
 			index: len(s.Redirs) - 1,
@@ -1275,7 +1279,6 @@ func (p *parser) letStmt() (ls LetStmt) {
 		}
 		ls.Exprs = append(ls.Exprs, x)
 	}
-	p.stopNewline = false
 	p.popStop()
 	p.got(STOPPED)
 	return
@@ -1302,6 +1305,7 @@ func (p *parser) cmdOrFunc() Node {
 	for !p.peekStop() {
 		var w Word
 		switch {
+		case p.got(STOPPED):
 		case p.gotRedirect():
 		case p.gotWord(&w):
 			cmd.Args = append(cmd.Args, w)

print_test.go
@@ -181,21 +181,30 @@ func TestFprintWeirdFormat(t *testing.T) {
 			"a=(\nb\nc\n) foo",
 			"a=(\n\tb\n\tc\n) foo",
 		},
+		{
+			"foo <<EOF | `bar`\n3\nEOF",
+			"foo <<EOF | `bar`\n3\nEOF",
+		},
 	}
 
 	for i, tc := range weirdFormats {
-		t.Run(fmt.Sprintf("%03d", i), func(t *testing.T) {
-			prog, err := Parse(strings.NewReader(tc.in), "", ParseComments)
-			if err != nil {
-				t.Fatal(err)
-			}
-			want := tc.want + "\n"
-			got := strFprint(prog, 0)
-			if got != want {
-				t.Fatalf("Fprint mismatch:\nin:\n%s\nwant:\n%sgot:\n%s",
-					tc.in, want, got)
-			}
-		})
+		for j, s := range [...]string{"", "\n"} {
+			t.Run(fmt.Sprintf("%03d-%d", i, j), func(t *testing.T) {
+				in := s + tc.in + s
+				prog, err := Parse(strings.NewReader(in), "",
+					ParseComments)
+				if err != nil {
+					t.Fatal(err)
+				}
+				want := tc.want + "\n"
+				got := strFprint(prog, 0)
+				if got != want {
+					t.Fatalf("Fprint mismatch:\n"+
+						"in:\n%s\nwant:\n%sgot:\n%s",
+						in, want, got)
+				}
+			})
+		}
 	}
 }
 

commit 9bf2c41c6f14b493897ce19ff6e28ad510fe2c6c

parse: fix regression introduced in ca2a636b
We should indeed be setting stopNewline back to false if the let
statement is done. If it wasn't finished by a newline, such as the added
test case, it will mean that a following statement after a semicolon
might not be terminated correctly.

parse.go
@@ -1259,6 +1259,7 @@ func (p *parser) letStmt() (ls LetStmt) {
 		}
 		ls.Exprs = append(ls.Exprs, x)
 	}
+	p.stopNewline = false
 	p.popStop()
 	p.got(STOPPED)
 	return

ast_test.go
@@ -1862,6 +1862,7 @@ var astTests = []testCase{
 		[]string{
 			"let i++\nfoo=(bar)",
 			"let i++; foo=(bar)",
+			"let i++; foo=(bar)\n",
 		},
 		[]Stmt{
 			stmt(LetStmt{Exprs: []Node{