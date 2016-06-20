mvdan/sh

commit 7c18c097cf6dbc37c259fea4ba6f36ddefb6725c

parse: rework space skipping logic
Also, be consistent about skipping spaces and \\\n in quotes.

Parse/Whitespace-4     4.56µs ± 2%    4.38µs ± 2%  -3.83%        (p=0.000 n=10+10)

parse.go
@@ -219,10 +219,13 @@ func (p *parser) next() {
 			p.lpos, p.pos = p.pos, p.npos
 			return
 		}
-		if q != DQUOTE && b == '\\' && p.readOnlyStr("\\\n") {
+		if q == DQUOTE || q == SQUOTE || q == RBRACE || q == QUO {
+			break
+		}
+		if b == '\\' && p.readOnlyStr("\\\n") {
 			continue
 		}
-		if q == DQUOTE || q == SQUOTE || q == RBRACE || q == QUO || !space(b) {
+		if !space(b) {
 			break
 		}
 		if p.stopNewline && b == '\n' {
@@ -231,10 +234,12 @@ func (p *parser) next() {
 			return
 		}
 		p.readByte()
-		p.spaced = true
-		if b == '\n' {
-			p.newLine = true
-			p.doHeredocs()
+		if !p.spaced {
+			p.spaced = true
+			if b == '\n' {
+				p.newLine = true
+				p.doHeredocs()
+			}
 		}
 	}
 	p.lpos, p.pos = p.pos, p.npos

commit e4f0cca9393ed5db51f565a10bf02308cf668084

parse: fix p.newLine regression in 7c18c097

parse.go
@@ -240,12 +240,10 @@ func (p *parser) next() {
 			return
 		}
 		p.readByte()
-		if !p.spaced {
-			p.spaced = true
-			if b == '\n' {
-				p.newLine = true
-				p.doHeredocs()
-			}
+		p.spaced = true
+		if b == '\n' {
+			p.newLine = true
+			p.doHeredocs()
 		}
 	}
 	p.advance(b, q)

ast_test.go
@@ -132,6 +132,7 @@ var astTests = []testCase{
 		[]string{
 			"if a; then b; fi",
 			"if a\nthen\nb\nfi",
+			"if a \nthen\nb\nfi",
 		},
 		&IfClause{
 			Cond:      &StmtCond{Stmts: litStmts("a")},