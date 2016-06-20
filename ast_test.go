mvdan/sh

commit 6eac1dcae6bd5772d47ec91ab0bdb7a4e3da571c

parse: only check against $" if quoted in it
name                 old time/op    new time/op    delta
Parse/Empty-4          1.20µs ± 6%    1.20µs ± 8%    ~           (p=0.726 n=10+10)
Parse/Whitespace-4     6.91µs ± 3%    6.92µs ± 2%    ~           (p=0.684 n=10+10)
Parse/Comment-4        1.73µs ± 4%    1.74µs ± 4%    ~            (p=0.618 n=9+10)
Parse/LongLit-4        10.5µs ± 3%    10.5µs ± 2%    ~           (p=0.796 n=10+10)
Parse/Cmds-4           21.8µs ± 5%    21.4µs ± 5%    ~           (p=0.143 n=10+10)
Parse/SglQuoted-4      3.07µs ± 3%    3.00µs ± 4%    ~           (p=0.089 n=10+10)
Parse/DblQuoted-4      6.53µs ± 2%    6.35µs ± 2%  -2.72%        (p=0.000 n=10+10)
Parse/NestedStmts-4    16.3µs ± 2%    16.2µs ± 5%    ~           (p=0.481 n=10+10)
Parse/Clauses-4        13.3µs ± 1%    13.2µs ± 2%    ~             (p=0.050 n=9+9)
Parse/CmdSubst-4       14.0µs ± 5%    13.4µs ± 1%  -3.79%         (p=0.000 n=10+8)
Parse/Binary-4         10.2µs ± 2%    10.1µs ± 2%    ~           (p=0.565 n=10+10)
Parse/Redirect-4       10.7µs ± 3%    10.5µs ± 1%    ~            (p=0.083 n=10+8)
Parse/Arithm-4         14.6µs ± 3%    14.4µs ± 2%  -1.44%        (p=0.012 n=10+10)

parse.go
@@ -291,7 +291,11 @@ func (p *parser) readLitBytes() (bs []byte) {
 			if b == '\'' {
 				return
 			}
-		case b == '$' && !p.willRead(`$"`) && !p.willRead(`$'`), b == '`':
+		case q == DQUOTE:
+			if b == '"' || (b == '$' && !p.willRead(`$"`)) {
+				return
+			}
+		case b == '$', b == '`':
 			return
 		case q == RBRACE:
 			if b == '}' || b == '"' {
@@ -301,10 +305,6 @@ func (p *parser) readLitBytes() (bs []byte) {
 			return
 		case q == QUO:
 			if b == '/' || b == '}' {
-				return
-			}
-		case q == DQUOTE:
-			if b == '"' {
 				return
 			}
 		case regOps(b), space(b):

ast_test.go
@@ -1085,6 +1085,20 @@ var astTests = []testCase{
 		}),
 	},
 	{
+		[]string{"${foo:=b${c}`d`}"},
+		word(&ParamExp{
+			Param: *lit("foo"),
+			Exp: &Expansion{
+				Op:   CASSIGN,
+				Word: *word(
+					lit("b"),
+					&ParamExp{Param: *lit("c")},
+					bckQuoted(litStmt("d")),
+				),
+			},
+		}),
+	},
+	{
 		[]string{`${foo?"${bar}"}`},
 		word(&ParamExp{
 			Param: *lit("foo"),

commit a277fcadd4079c398edd1dffbc682979900ff235

parse: fix regression introduced in 6eac1dca
We must stop reading lits if we find '`' inside double quotes.

parse.go
@@ -311,14 +311,14 @@ func (p *parser) readLitBytes() (bs []byte) {
 			if b == '\'' {
 				return
 			}
+		case b == '`':
+			p.willBreakWord = true
+			return
 		case q == DQUOTE:
 			if b == '"' || (b == '$' && !p.willReadStr(`$"`)) {
 				return
 			}
-		case b == '$', b == '`':
-			if b == '`' {
-				p.willBreakWord = true
-			}
+		case b == '$':
 			return
 		case q == RBRACE:
 			if b == '}' || b == '"' {

ast_test.go
@@ -2193,6 +2193,17 @@ var astTests = []testCase{
 			}))),
 		},
 	},
+	{
+		[]string{"\"a`\"\"`\""},
+		word(dblQuoted(
+			lit("a"),
+			bckQuoted(
+				stmt(call(
+					*word(dblQuoted()),
+				)),
+			),
+		)),
+	},
 }
 
 func fullProg(v interface{}) *File {