e4be7ca62d721957dd4268a4fd7ac48f786f5b4b
Daniel Martí <mvdan@mvdan.cc>
2016-11-29 01:15:03 +0000

    cmd/shfmt: fix env argument spacing regexp
    In particular, we should allow any whitespace and require at least one
    character to separate the sh/bash argument.

--- cmd/shfmt/main.go
+++ cmd/shfmt/main.go
@@ -73,7 +73,7 @@ func formatStdin() error {
 
 var (
 	shellFile    = regexp.MustCompile(`\.(sh|bash)$`)
-	validShebang = regexp.MustCompile(`^#!\s?/(usr/)?bin/(env *)?(sh|bash)`)
+	validShebang = regexp.MustCompile(`^#!\s?/(usr/)?bin/(env\s+)?(sh|bash)`)
 	vcsDir       = regexp.MustCompile(`^\.(git|svn|hg)$`)
 )
 
