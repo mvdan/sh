mvdan/sh

commit 98d60705ab6e2553d0b51c22e24ee4d8a0160b28

syntax: add support for bash 4.4's ${foo@bar}
For now, just record the operator as a word.

syntax/parser.go
@@ -910,6 +910,8 @@ func (p *parser) paramExp() *ParamExp {
 	old := p.preNested(paramExpName)
 	p.next()
 	switch p.tok {
+	case at:
+		p.tok, p.val = _Lit, "@"
 	case dblHash:
 		p.tok = hash
 		p.npos--
@@ -1002,9 +1004,9 @@ func (p *parser) paramExp() *ParamExp {
 				p.followErrExp(colonPos, ":")
 			}
 		}
-	case caret, dblCaret, comma, dblComma:
+	case caret, dblCaret, comma, dblComma, at:
 		if !p.bash() {
-			p.curErr("case expansions are a bash feature")
+			p.curErr("this expansion operator is a bash feature")
 		}
 		fallthrough
 	default:

syntax/filetests_test.go
@@ -1845,6 +1845,24 @@ var fileTests = []testCase{
 		),
 	},
 	{
+		Strs: []string{`${a@E} ${b@a}`},
+		bash: call(
+			word(&ParamExp{Param: lit("a"),
+				Exp: &Expansion{
+					Op:   OtherParamOps,
+					Word: litWord("E"),
+				},
+			}),
+			word(&ParamExp{Param: lit("b"),
+				Exp: &Expansion{
+					Op:   OtherParamOps,
+					Word: litWord("a"),
+				},
+			}),
+		),
+		minBash: 44,
+	},
+	{
 		Strs: []string{`${#foo}`},
 		common: &ParamExp{
 			Length: true,

syntax/lexer.go
@@ -20,7 +20,7 @@ func regOps(r rune) bool {
 // tokenize these inside parameter expansions
 func paramOps(r rune) bool {
 	switch r {
-	case '}', '#', ':', '-', '+', '=', '?', '%', '[', ']', '/', '^', ',':
+	case '}', '#', ':', '-', '+', '=', '?', '%', '[', ']', '/', '^', ',', '@':
 		return true
 	}
 	return false
@@ -451,6 +451,12 @@ func (p *parser) paramToken(r rune) token {
 	case '[':
 		p.rune()
 		return leftBrack
+	case '/':
+		if p.rune() == '/' {
+			p.rune()
+			return dblSlash
+		}
+		return slash
 	case '^':
 		if p.rune() == '^' {
 			p.rune()
@@ -463,12 +469,9 @@ func (p *parser) paramToken(r rune) token {
 			return dblComma
 		}
 		return comma
-	default: // '/'
-		if p.rune() == '/' {
-			p.rune()
-			return dblSlash
-		}
-		return slash
+	default: // '@'
+		p.rune()
+		return at
 	}
 }
 
@@ -674,7 +677,7 @@ loop:
 			if p.quote&allArithmExpr != 0 || p.quote&allParamReg != 0 {
 				break loop
 			}
-		case '#', '[':
+		case '#', '[', '@':
 			if p.quote&allParamReg != 0 {
 				break loop
 			}

syntax/parser_test.go
@@ -975,6 +975,14 @@ var bashTests = []errorCase{
 		"echo ${foo:1:2",
 		`1:6: reached EOF without matching ${ with }`,
 	},
+	{
+		"echo ${foo,",
+		`1:6: reached EOF without matching ${ with }`,
+	},
+	{
+		"echo ${foo@",
+		`1:6: reached EOF without matching ${ with }`,
+	},
 }
 
 var posixTests = []errorCase{
@@ -1036,7 +1044,11 @@ var posixTests = []errorCase{
 	},
 	{
 		"echo ${foo,bar} #INVBASH --posix is wrong",
-		`1:11: case expansions are a bash feature`,
+		`1:11: this expansion operator is a bash feature`,
+	},
+	{
+		"echo ${foo@bar} #INVBASH --posix is wrong",
+		`1:11: this expansion operator is a bash feature`,
 	},
 }
 

syntax/token_string.go
@@ -4,9 +4,9 @@ package syntax
 
 import "fmt"
 
-const _token_name = "illegalTokEOFLitLitWord'\"`&&&||||&$$'$\"${$[$($(([(((}])));;;;&;;&!++--***==!=<=>=+=-=*=/=%=&=|=^=<<=>>=>>><<><&>&>|<<<<-<<<&>&>><(>(+:+-:-?:?=:=%%%###^^^,,,///:-e-f-d-c-b-p-S-L-g-u-r-w-x-s-t-z-n-o-v-R=~-nt-ot-ef-eq-ne-le-ge-lt-gt?(*(+(@(!("
+const _token_name = "illegalTokEOFLitLitWord'\"`&&&||||&$$'$\"${$[$($(([(((}])));;;;&;;&!++--***==!=<=>=+=-=*=/=%=&=|=^=<<=>>=>>><<><&>&>|<<<<-<<<&>&>><(>(+:+-:-?:?=:=%%%###^^^,,,@///:-e-f-d-c-b-p-S-L-g-u-r-w-x-s-t-z-n-o-v-R=~-nt-ot-ef-eq-ne-le-ge-lt-gt?(*(+(@(!("
 
-var _token_index = [...]uint8{0, 10, 13, 16, 23, 24, 25, 26, 27, 29, 31, 32, 34, 35, 37, 39, 41, 43, 45, 48, 49, 50, 52, 53, 54, 55, 57, 58, 60, 62, 65, 66, 68, 70, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 100, 103, 104, 106, 107, 109, 111, 113, 115, 117, 120, 123, 125, 128, 130, 132, 133, 135, 136, 138, 139, 141, 142, 144, 145, 147, 148, 150, 151, 153, 154, 156, 157, 159, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 205, 208, 211, 214, 217, 220, 223, 226, 229, 231, 233, 235, 237, 239}
+var _token_index = [...]uint8{0, 10, 13, 16, 23, 24, 25, 26, 27, 29, 31, 32, 34, 35, 37, 39, 41, 43, 45, 48, 49, 50, 52, 53, 54, 55, 57, 58, 60, 62, 65, 66, 68, 70, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 100, 103, 104, 106, 107, 109, 111, 113, 115, 117, 120, 123, 125, 128, 130, 132, 133, 135, 136, 138, 139, 141, 142, 144, 145, 147, 148, 150, 151, 153, 154, 156, 157, 158, 160, 161, 163, 165, 167, 169, 171, 173, 175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 195, 197, 199, 201, 203, 206, 209, 212, 215, 218, 221, 224, 227, 230, 232, 234, 236, 238, 240}
 
 func (i token) String() string {
 	if i >= token(len(_token_index)-1) {

syntax/tokens.go
@@ -98,6 +98,7 @@ const (
 	dblCaret // ^^
 	comma    // ,
 	dblComma // ,,
+	at       // @
 	slash    // /
 	dblSlash // //
 	colon    // :
@@ -211,6 +212,7 @@ const (
 	UpperAll
 	LowerFirst
 	LowerAll
+	OtherParamOps
 )
 
 type UnAritOperator token

commit 5a56c39a735258f38e67a44414f7a80d032ad639

syntax: parse ${foo[@]} properly again
This is a regression from 98d60705, which introduced support for
${foo@bar}. '@' is now a token in this context, so make sure it's
handled separately like '*'.

syntax/parser.go
@@ -949,8 +949,12 @@ func (p *parser) paramExp() *ParamExp {
 		}
 		lpos := p.pos
 		p.quote = paramExpInd
-		if p.next(); p.tok == star {
+		p.next()
+		switch p.tok {
+		case star:
 			p.tok, p.val = _LitWord, "*"
+		case at:
+			p.tok, p.val = _LitWord, "@"
 		}
 		pe.Ind = &Index{
 			Expr: p.arithmExpr(leftBrack, lpos, 0, false, false),

syntax/filetests_test.go
@@ -1637,6 +1637,13 @@ var fileTests = []testCase{
 		},
 	},
 	{
+		Strs: []string{`${foo[@]}`},
+		bash: &ParamExp{
+			Param: lit("foo"),
+			Ind:   &Index{Expr: litWord("@")},
+		},
+	},
+	{
 		Strs: []string{`${foo[*]-etc}`},
 		bash: &ParamExp{
 			Param: lit("foo"),