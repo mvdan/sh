327f43c2839a48611852860f3fc9622eb3550ac6
Daniel Martí <mvdan@mvdan.cc>
2017-04-18 12:22:08 +0100

    interp: add env tests with duplicates and globals

--- interp/interp_test.go
+++ interp/interp_test.go
@@ -103,6 +103,8 @@ var fileCases = []struct {
 	{"foo=bar; foo=x true; echo $foo", "bar\n"},
 	{"foo=bar; env | grep foo", "exit status 1"},
 	{"foo=bar env | grep foo", "foo=bar\n"},
+	{"foo=a foo=b env | grep foo", "foo=b\n"},
+	{"env | grep INTERP_GLOBAL", "INTERP_GLOBAL=value\n"},
 
 	// special vars
 	{"echo $?; false; echo $?", "0\n1\n"},
