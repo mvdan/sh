mvdan/sh

commit af2b77f10e9e4d608dd500f73ef41c13d38aedb4

syntax: fix multi-byte rune positions for good
Our code was messy. This was evident in our hard-coding of adding and
removing 1 to the column byte number, notably in the two lines with
TODOs.

Instead, properly document what p.bsp, p.r and p.npos are. And add p.w
to keep track of the width of p.r. Only advance that at the beginning of
p.rune, so as to keep p.npos correct.

The benchmarks get slightly slower in some cases. But correctness takes
priority.

name                   old time/op    new time/op    delta
Parse/LongStrs-4         8.01µs ± 0%    8.37µs ± 0%  +4.45%  (p=0.002 n=6+6)
Parse/Cmds+Nested-4      11.2µs ± 1%    11.1µs ± 0%  -0.52%  (p=0.009 n=6+6)
Parse/Vars+Clauses-4     9.48µs ± 1%    9.53µs ± 1%    ~     (p=0.132 n=6+6)
Parse/Binary+Redirs-4    10.9µs ± 1%    11.1µs ± 0%  +2.08%  (p=0.002 n=6+6)

syntax/lexer.go
@@ -57,21 +57,22 @@ func wordBreak(r rune) bool {
 
 func (p *Parser) rune() rune {
 retry:
+	if p.r == '\n' {
+		// p.r instead of b so that newline
+		// character positions don't have col 0.
+		p.npos.line++
+		p.npos.col = 1
+	} else {
+		p.npos.col += p.w
+	}
 	if p.bsp < len(p.bs) {
 		if b := p.bs[p.bsp]; b < utf8.RuneSelf {
 			p.bsp++
-			if p.r == '\n' {
-				// p.r instead of b so that newline
-				// character positions don't have col 0.
-				p.npos.line++
-				p.npos.col = 0
-			}
-			p.npos.col++
 			if p.litBs != nil {
 				p.litBs = append(p.litBs, b)
 			}
 			r := rune(b)
-			p.r = r
+			p.r, p.w = r, 1
 			return r
 		}
 		if p.bsp+utf8.UTFMax >= len(p.bs) {
@@ -85,10 +86,10 @@ retry:
 			p.litBs = append(p.litBs, p.bs[p.bsp:p.bsp+w]...)
 		}
 		p.bsp += w
-		p.npos.col += uint16(w)
 		if p.r == utf8.RuneError && w == 1 {
 			p.posErr(p.npos, "invalid UTF-8 encoding")
 		}
+		p.w = uint16(w)
 	} else {
 		if p.r == utf8.RuneSelf {
 		} else if p.fill(); p.bs == nil {
@@ -134,9 +135,7 @@ func (p *Parser) fill() {
 
 func (p *Parser) nextKeepSpaces() {
 	r := p.r
-	if p.pos = p.getPos(); r > utf8.RuneSelf {
-		p.pos = posAddCol(p.pos, -1) // TODO
-	}
+	p.pos = p.getPos()
 	switch p.quote {
 	case paramExpRepl:
 		switch r {
@@ -225,9 +224,7 @@ skipSpace:
 			break skipSpace
 		}
 	}
-	if p.pos = p.getPos(); r > utf8.RuneSelf {
-		p.pos = posAddCol(p.pos, -1) // TODO
-	}
+	p.pos = p.getPos()
 	switch {
 	case p.quote&allRegTokens != 0:
 		switch r {

syntax/parser.go
@@ -54,8 +54,9 @@ func (p *Parser) Parse(src io.Reader, name string) (*File, error) {
 type Parser struct {
 	src io.Reader
 	bs  []byte // current chunk of read bytes
-	bsp int    // pos within chunk for the next rune
-	r   rune
+	bsp int    // pos within chunk for the rune after r
+	r   rune   // next rune
+	w   uint16 // width of r
 
 	f *File
 
@@ -70,7 +71,7 @@ type Parser struct {
 
 	offs int
 	pos  Pos // position of tok
-	npos Pos // next position
+	npos Pos // next position (of r)
 
 	quote   quoteState // current lexer state
 	eqlOffs int        // position of '=' in val (a literal)
@@ -107,15 +108,16 @@ const bufSize = 1 << 10
 func (p *Parser) reset() {
 	p.bs, p.bsp = nil, 0
 	p.offs = 0
-	p.npos = Pos{line: 1}
-	p.r, p.err, p.readErr = 0, nil, nil
+	p.npos = Pos{line: 1, col: 1}
+	p.r, p.w = 0, 0
+	p.err, p.readErr = nil, nil
 	p.quote, p.forbidNested = noState, false
 	p.heredocs, p.buriedHdocs = p.heredocs[:0], 0
 	p.accComs, p.curComs = nil, &p.accComs
 }
 
 func (p *Parser) getPos() Pos {
-	p.npos.offs = uint32(p.offs + p.bsp - 1)
+	p.npos.offs = uint32(p.offs + p.bsp - int(p.w))
 	return p.npos
 }
 

commit e639c1a4220c0d116191778c8b70b7899cb48c1d

syntax: don't advance npos when retrying p.rune
Otherwise, we might e.g. advance a line with a newline twice. Added
test.

Regression introduced very recently in af2b77f1.

syntax/lexer.go
@@ -56,7 +56,6 @@ func wordBreak(r rune) bool {
 }
 
 func (p *Parser) rune() rune {
-retry:
 	if p.r == '\n' {
 		// p.r instead of b so that newline
 		// character positions don't have col 0.
@@ -65,6 +64,7 @@ retry:
 	} else {
 		p.npos.col += p.w
 	}
+retry:
 	if p.bsp < len(p.bs) {
 		if b := p.bs[p.bsp]; b < utf8.RuneSelf {
 			p.bsp++

syntax/printer_test.go
@@ -80,6 +80,8 @@ func TestPrintWeirdFormat(t *testing.T) {
 		{strings.Repeat(" ", bufSize-10) + "${a/b//○}", "${a/b//○}"},
 		// multiple p.fill calls
 		{"a" + strings.Repeat(" ", bufSize*4) + "b", "a b"},
+		// newline at the beginning of second chunk
+		{"a" + strings.Repeat(" ", bufSize-2) + "\nb", "a\nb"},
 		{"foo; bar", "foo\nbar"},
 		{"foo\n\n\nbar", "foo\n\nbar"},
 		{"foo\n\n", "foo"},