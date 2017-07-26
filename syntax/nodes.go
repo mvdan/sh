mvdan/sh

commit 1cf7c9e01b7846b4d1c2188f8f41ebc46c4d7734

syntax: keep ;; in a separate line after a comment
This edge case with an inline comment and no statements was broken in
the StmtList refactor.

The fix is a bit verbose, but we can overhaul this logic later once the
code is right and the tests are in place.

Fixes #134.

syntax/printer.go
@@ -704,6 +704,9 @@ func (p *Printer) command(cmd Command, redirs []*Redirect) (startRedirs int) {
 			p.WriteByte(')')
 			p.wantSpace = true
 			sep := len(ci.Stmts) > 1 || ci.StmtList.pos().Line() > p.line
+			if ci.OpPos != x.Esac && ci.OpPos.Line() > p.line {
+				sep = true
+			}
 			sl := ci.StmtList
 			p.nestedStmts(sl, Pos{})
 			p.level++

syntax/printer_test.go
@@ -355,6 +355,7 @@ func TestPrintWeirdFormat(t *testing.T) {
 		samePrint("case $i in\n#bef\n1) ;; #inl\nesac"),
 		samePrint("case $i in\n1) ;; #inl1\n2) ;; #inl2\nesac"),
 		samePrint("case $i in\n#bef\n1) #inl\n\tfoo\n\t;;\nesac"),
+		samePrint("case $i in\n1) #inl\n\t;;\nesac"),
 		{
 			"a=(\nb\nc\n) foo",
 			"a=(\n\tb\n\tc\n) foo",

commit 23f57494332cd258c69085f52826a01ec1f6dab8

syntax: don't separate ;; after multi-line cmd
This is an unintended regression from 1cf7c9e0. The new behavior isn't
particularly bad per se, but the old one is slightly better and we don't
want to change the format for no reason.

syntax/nodes.go
@@ -40,6 +40,16 @@ func (s StmtList) pos() Pos {
 	return Pos{}
 }
 
+func (s StmtList) end() Pos {
+	if len(s.Last) > 0 {
+		return s.Last[len(s.Last)-1].End()
+	}
+	if len(s.Stmts) > 0 {
+		return s.Stmts[len(s.Stmts)-1].End()
+	}
+	return Pos{}
+}
+
 func (s StmtList) empty() bool {
 	return len(s.Stmts) == 0 && len(s.Last) == 0
 }

syntax/printer.go
@@ -705,7 +705,8 @@ func (p *Printer) command(cmd Command, redirs []*Redirect) (startRedirs int) {
 			p.WriteByte(')')
 			p.wantSpace = true
 			sep := len(ci.Stmts) > 1 || ci.StmtList.pos().Line() > p.line
-			if ci.OpPos != x.Esac && ci.OpPos.Line() > p.line {
+			if ci.OpPos != x.Esac && !ci.StmtList.empty() &&
+				ci.OpPos.Line() > ci.StmtList.end().Line() {
 				sep = true
 			}
 			sl := ci.StmtList

syntax/printer_test.go
@@ -358,6 +358,7 @@ func TestPrintWeirdFormat(t *testing.T) {
 		samePrint("case $i in\n1) ;; #inl1\n2) ;; #inl2\nesac"),
 		samePrint("case $i in\n#bef\n1) #inl\n\tfoo\n\t;;\nesac"),
 		samePrint("case $i in\n1) #inl\n\t;;\nesac"),
+		samePrint("case $i in\n1) a \\\n\tb ;;\nesac"),
 		{
 			"a=(\nb\nc\n) foo",
 			"a=(\n\tb\n\tc\n) foo",