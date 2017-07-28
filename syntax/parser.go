mvdan/sh

commit 7f14009f44bf2a0128bb86d85f8c03ba72e01384

syntax: don't start compound commands after redirs
Following the POSIX spec. For example, we weren't erroring on inputs
like:

	>foo { bar; }

Needed to modify p.callExpr to allow returning untyped nils. Not many
changes were needed, and there's no performance change.

syntax/parser.go
@@ -1375,20 +1375,6 @@ func (p *Parser) getStmt(readEnd, binCmd bool) (s *Stmt, gotEnd bool) {
 }
 
 func (p *Parser) gotStmtPipe(s *Stmt) *Stmt {
-preLoop:
-	for {
-		switch p.tok {
-		case rdrOut, appOut, rdrIn, dplIn, dplOut, clbOut, rdrInOut,
-			hdoc, dashHdoc, wordHdoc, rdrAll, appAll, _LitRedir:
-			p.doRedirect(s)
-		default:
-			break preLoop
-		}
-		switch {
-		case p.newLine, p.tok == _EOF, p.tok == semicolon:
-			return s
-		}
-	}
 	switch p.tok {
 	case _LitWord:
 		switch p.val {
@@ -1458,7 +1444,7 @@ preLoop:
 			break
 		}
 		if p.hasValidIdent() {
-			s.Cmd = p.callExpr(s, nil)
+			s.Cmd = p.callExpr(s, nil, true)
 			break
 		}
 		name := p.lit(p.pos, p.val)
@@ -1469,8 +1455,16 @@ preLoop:
 			}
 			s.Cmd = p.funcDecl(name, name.ValuePos)
 		} else {
-			s.Cmd = p.callExpr(s, p.word(p.wps(name)))
+			s.Cmd = p.callExpr(s, p.word(p.wps(name)), false)
 		}
+	case rdrOut, appOut, rdrIn, dplIn, dplOut, clbOut, rdrInOut,
+		hdoc, dashHdoc, wordHdoc, rdrAll, appAll, _LitRedir:
+		p.doRedirect(s)
+		switch {
+		case p.newLine, p.tok == _EOF, p.tok == semicolon:
+			return s
+		}
+		s.Cmd = p.callExpr(s, nil, false)
 	case bckQuote:
 		if p.quote == subCmdBckquo {
 			return s
@@ -1480,14 +1474,14 @@ preLoop:
 		sglQuote, dollSglQuote, dblQuote, dollDblQuote, dollBrack,
 		globQuest, globStar, globPlus, globAt, globExcl:
 		if p.hasValidIdent() {
-			s.Cmd = p.callExpr(s, nil)
+			s.Cmd = p.callExpr(s, nil, true)
 			break
 		}
 		w := p.word(p.wordParts())
 		if p.gotSameLine(leftParen) && p.err == nil {
 			p.posErr(w.Pos(), "invalid func name")
 		}
-		s.Cmd = p.callExpr(s, w)
+		s.Cmd = p.callExpr(s, w, false)
 	case leftParen:
 		s.Cmd = p.subshell()
 	case dblLeftParen:
@@ -1984,10 +1978,12 @@ func (p *Parser) bashFuncDecl() *FuncDecl {
 	return p.funcDecl(name, fpos)
 }
 
-func (p *Parser) callExpr(s *Stmt, w *Word) *CallExpr {
+func (p *Parser) callExpr(s *Stmt, w *Word, assign bool) Command {
 	ce := p.call(w)
 	if w == nil {
 		ce.Args = ce.Args[:0]
+	}
+	if assign {
 		for {
 			ce.Assigns = append(ce.Assigns, p.getAssign(true))
 			if p.newLine || (p.tok != _Lit && p.tok != _LitWord) ||
@@ -1996,14 +1992,12 @@ func (p *Parser) callExpr(s *Stmt, w *Word) *CallExpr {
 			}
 		}
 	}
+loop:
 	for !p.newLine {
 		switch p.tok {
 		case _EOF, semicolon, and, or, andAnd, orOr, orAnd,
 			dblSemicolon, semiAnd, dblSemiAnd, semiOr:
-			if len(ce.Args) == 0 {
-				ce.Args = nil
-			}
-			return ce
+			break loop
 		case _LitWord:
 			ce.Args = append(ce.Args, p.word(
 				p.wps(p.lit(p.pos, p.val)),
@@ -2011,7 +2005,7 @@ func (p *Parser) callExpr(s *Stmt, w *Word) *CallExpr {
 			p.next()
 		case bckQuote:
 			if p.quote == subCmdBckquo {
-				return ce
+				break loop
 			}
 			fallthrough
 		case _Lit, dollBrace, dollDblParen, dollParen, dollar, cmdIn, cmdOut,
@@ -2025,12 +2019,15 @@ func (p *Parser) callExpr(s *Stmt, w *Word) *CallExpr {
 			p.curErr("%s can only be used to open an arithmetic cmd", p.tok)
 		case rightParen:
 			if p.quote == subCmd {
-				return ce
+				break loop
 			}
 			fallthrough
 		default:
 			p.curErr("a command can only contain words and redirects")
 		}
+	}
+	if len(ce.Assigns) == 0 && len(ce.Args) == 0 {
+		return nil
 	}
 	if len(ce.Args) == 0 {
 		ce.Args = nil

syntax/parser_test.go
@@ -486,6 +486,18 @@ var shellTests = []errorCase{
 		common: `1:13: "then" can only be used in an if`,
 	},
 	{
+		in:     ">f { foo; }",
+		common: `1:11: } can only be used to close a block`,
+	},
+	{
+		in:     ">f foo() { bar; }",
+		common: `1:7: a command can only contain words and redirects`,
+	},
+	{
+		in:     ">f if foo; then bar; fi",
+		common: `1:12: "then" can only be used in an if`,
+	},
+	{
 		in:     "'",
 		common: `1:1: reached EOF without closing quote '`,
 	},

commit cf9285ffee9bee816bdf5a7414b5260773871f28

syntax: fix assign crash introduced in 7f14009f
With test. Found via fuzzing.

syntax/parser.go
@@ -1473,7 +1473,7 @@ func (p *Parser) gotStmtPipe(s *Stmt) *Stmt {
 	case _Lit, dollBrace, dollDblParen, dollParen, dollar, cmdIn, cmdOut,
 		sglQuote, dollSglQuote, dblQuote, dollDblQuote, dollBrack,
 		globQuest, globStar, globPlus, globAt, globExcl:
-		if p.hasValidIdent() {
+		if p.tok == _Lit && p.hasValidIdent() {
 			s.Cmd = p.callExpr(s, nil, true)
 			break
 		}

syntax/parser_test.go
@@ -1597,6 +1597,10 @@ var shellTests = []errorCase{
 		posix: `1:5: arrays are a bash feature`,
 	},
 	{
+		in:     "a=$c\n'",
+		common: `2:1: reached EOF without closing quote '`,
+	},
+	{
 		in:    "echo ${foo[1]}",
 		posix: `1:11: arrays are a bash feature`,
 	},