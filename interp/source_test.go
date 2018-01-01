04387a2002c456143ccd795158dcfe45fcf10351
Daniel Martí <mvdan@mvdan.cc>
2018-01-01 17:36:54 +0000

    interp: add static source helpers
    To make it trivially easy to get the variables declared in a shell file,
    such as config files or PKGBUILDs.
    
    Updates #198.

---
+++ interp/source_test.go
@@ -0,0 +1,97 @@
+// Copyright (c) 2018, Daniel Martí <mvdan@mvdan.cc>
+// See LICENSE for licensing information
+
+package interp
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+
+	"mvdan.cc/sh/syntax"
+
+	"github.com/kr/pretty"
+)
+
+var mapTests = []struct {
+	in   string
+	want map[string]Variable
+}{
+	{
+		"a=x; b=y",
+		map[string]Variable{
+			"a": {Value: StringVal("x")},
+			"b": {Value: StringVal("y")},
+		},
+	},
+	{
+		"a=x; a=y; X=(a b c)",
+		map[string]Variable{
+			"a": {Value: StringVal("y")},
+			"X": {Value: IndexArray{"a", "b", "c"}},
+		},
+	},
+	{
+		"a=$(echo foo | sed 's/o/a/g')",
+		map[string]Variable{
+			"a": {Value: StringVal("faa")},
+		},
+	},
+}
+
+var errTests = []struct {
+	in   string
+	want string
+}{
+	{
+		"rm -rf /",
+		"not in whitelist: rm",
+	},
+	{
+		"cat secret >some-file",
+		"cannot open path",
+	},
+}
+
+func TestSource(t *testing.T) {
+	for i := range mapTests {
+		t.Run(fmt.Sprintf("%02d", i), func(t *testing.T) {
+			tc := mapTests[i]
+			t.Parallel()
+			p := syntax.NewParser()
+			file, err := p.Parse(strings.NewReader(tc.in), "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			got, err := SourceNode(file)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatal(strings.Join(pretty.Diff(tc.want, got), "\n"))
+			}
+		})
+	}
+}
+
+func TestSourceErr(t *testing.T) {
+	for i := range errTests {
+		t.Run(fmt.Sprintf("%02d", i), func(t *testing.T) {
+			tc := errTests[i]
+			t.Parallel()
+			p := syntax.NewParser()
+			file, err := p.Parse(strings.NewReader(tc.in), "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = SourceNode(file)
+			if err == nil {
+				t.Fatal("wanted non-nil error")
+			}
+			if !strings.Contains(err.Error(), tc.want) {
+				t.Fatalf("error %q does not match %q", err, tc.want)
+			}
+		})
+	}
+}
