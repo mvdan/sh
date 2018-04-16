2e3aca463a88db7f5a1a077a1d1de948f7c694cb
Daniel Martí <mvdan@mvdan.cc>
2018-04-16 15:34:06 +0900

    syntax: add DebugPrint
    A simpler, smaller version of ast.Fprint but for our AST. Include an
    example too, which also serves as its test.
    
    Field filtering is left out, as it seems like an overkill feature for
    what is essentially a debugging function.
    
    The reflect code isn't very complex, as our AST doesn't contain certain
    types like maps and arrays, nor do any node structs contain unexported
    fields.

--- syntax/example_walk_test.go
+++ syntax/example_walk_test.go
@@ -26,3 +26,54 @@ func ExampleWalk() {
 	syntax.NewPrinter().Print(os.Stdout, f)
 	// Output: echo $FOO "and $BAR"
 }
+
+func ExampleDebugPrint() {
+	in := strings.NewReader(`echo 'foo'`)
+	f, err := syntax.NewParser().Parse(in, "")
+	if err != nil {
+		return
+	}
+	syntax.DebugPrint(os.Stdout, f)
+	// Output:
+	// *syntax.File {
+	// .  Name: ""
+	// .  StmtList: syntax.StmtList {
+	// .  .  Stmts: []*syntax.Stmt (len = 1) {
+	// .  .  .  0: *syntax.Stmt {
+	// .  .  .  .  Comments: []syntax.Comment (len = 0) {}
+	// .  .  .  .  Cmd: *syntax.CallExpr {
+	// .  .  .  .  .  Assigns: []*syntax.Assign (len = 0) {}
+	// .  .  .  .  .  Args: []*syntax.Word (len = 2) {
+	// .  .  .  .  .  .  0: *syntax.Word {
+	// .  .  .  .  .  .  .  Parts: []syntax.WordPart (len = 1) {
+	// .  .  .  .  .  .  .  .  0: *syntax.Lit {
+	// .  .  .  .  .  .  .  .  .  ValuePos: 1:1
+	// .  .  .  .  .  .  .  .  .  ValueEnd: 1:5
+	// .  .  .  .  .  .  .  .  .  Value: "echo"
+	// .  .  .  .  .  .  .  .  }
+	// .  .  .  .  .  .  .  }
+	// .  .  .  .  .  .  }
+	// .  .  .  .  .  .  1: *syntax.Word {
+	// .  .  .  .  .  .  .  Parts: []syntax.WordPart (len = 1) {
+	// .  .  .  .  .  .  .  .  0: *syntax.SglQuoted {
+	// .  .  .  .  .  .  .  .  .  Left: 1:6
+	// .  .  .  .  .  .  .  .  .  Right: 1:10
+	// .  .  .  .  .  .  .  .  .  Dollar: false
+	// .  .  .  .  .  .  .  .  .  Value: "foo"
+	// .  .  .  .  .  .  .  .  }
+	// .  .  .  .  .  .  .  }
+	// .  .  .  .  .  .  }
+	// .  .  .  .  .  }
+	// .  .  .  .  }
+	// .  .  .  .  Position: 1:1
+	// .  .  .  .  Semicolon: 0:0
+	// .  .  .  .  Negated: false
+	// .  .  .  .  Background: false
+	// .  .  .  .  Coprocess: false
+	// .  .  .  .  Redirs: []*syntax.Redirect (len = 0) {}
+	// .  .  .  }
+	// .  .  }
+	// .  .  Last: []syntax.Comment (len = 0) {}
+	// .  }
+	// }
+}
