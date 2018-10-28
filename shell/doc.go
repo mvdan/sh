0d0be94fa945288ab3b9f4ccff16b9a4b9527878
Daniel Martí <mvdan@mvdan.cc>
2018-10-28 22:45:46 +0000

    shell: stabilize API
    Now that we've figured out all the warts and clarified the dependencies
    on the interp package, we can safely freeze the API within v2.
    
    Updates #283.

--- shell/doc.go
+++ shell/doc.go
@@ -3,7 +3,4 @@
 
 // Package shell contains high-level features that use the syntax, expand, and
 // interp packages under the hood.
-//
-// This package is a work in progress and EXPERIMENTAL; its API is not
-// subject to the 2.x backwards compatibility guarantee.
 package shell
