1eb0ae59279b4ced7b6873c5c8f3b7bb685942d5
Daniel Martí <mvdan@mvdan.cc>
2018-10-29 20:07:17 +0000

    interp: stabilize API
    We're going to freeze v2 and start work on v3 (see #266), so it makes
    sense to stabilize the interp package now that we're reasonably happy
    with its API.
    
    The API is not perfect, but it's definitely better than earlier v2
    versions. And plenty of packages already depend on the existing API,
    anyway.
    
    Fixes #283.

--- interp/doc.go
+++ interp/doc.go
@@ -4,7 +4,4 @@
 // Package interp implements an interpreter that executes shell
 // programs. It aims to support POSIX, but its support is not complete
 // yet. It also supports some Bash features.
-//
-// This package is a work in progress and EXPERIMENTAL; its API is not
-// subject to the 2.x backwards compatibility guarantee.
 package interp
