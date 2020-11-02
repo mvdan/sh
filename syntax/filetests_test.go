mvdan/sh

commit 5bb742d102d2828766867c99f7fd72a3e3a0581c

syntax: support single quotes in parameter expansion extensions
We were incorrectly not tokenizing single quotes in the middle of those
words.

Fixes #587.

syntax/lexer.go
@@ -807,7 +807,7 @@ loop:
 		switch r {
 		case '\\': // escaped byte follows
 			p.rune()
-		case '"', '`', '$':
+		case '\'', '"', '`', '$':
 			tok = _Lit
 			break loop
 		case '}':
@@ -831,7 +831,7 @@ loop:
 			if p.quote&allParamReg != 0 {
 				break loop
 			}
-		case '\'', '+', '-', ' ', '\t', ';', '&', '>', '<', '|', '(', ')', '\n', '\r':
+		case '+', '-', ' ', '\t', ';', '&', '>', '<', '|', '(', ')', '\n', '\r':
 			if p.quote&allKeepSpaces == 0 {
 				break loop
 			}

syntax/filetests_test.go
@@ -2039,6 +2039,16 @@ var fileTests = []testCase{
 		},
 	},
 	{
+		Strs: []string{`${var#*'="'}`},
+		common: &ParamExp{
+			Param: lit("var"),
+			Exp: &Expansion{
+				Op:   RemSmallPrefix,
+				Word: word(lit("*"), sglQuoted(`="`)),
+			},
+		},
+	},
+	{
 		Strs: []string{`${foo%bar}${foo%%bar*}`},
 		common: word(
 			&ParamExp{

syntax/parser_test.go
@@ -942,7 +942,7 @@ var shellTests = []errorCase{
 	},
 	{
 		in:     `echo $((a'`,
-		common: `1:10: not a valid arithmetic operator: '`,
+		common: `1:10: reached EOF without closing quote '`,
 	},
 	{
 		in:     `echo $((a b"`,
@@ -1018,8 +1018,13 @@ var shellTests = []errorCase{
 		common: `1:13: = must follow a name #NOERR`,
 	},
 	{
-		in:     "echo $((1'2'))",
-		common: `1:10: not a valid arithmetic operator: '`,
+		in: "echo $((1'2'))",
+		// TODO: Take a look at this again, since this no longer fails
+		// after fixing github.com/mvdan/sh/issues/587.
+		// Note that Bash seems to treat code inside $(()) as if it were
+		// within double quotes, yet still requires single quotes to be
+		// matched.
+		// common: `1:10: not a valid arithmetic operator: '`,
 	},
 	{
 		in:     "<<EOF\n$(()a",

commit b1d056c32fce2c42d12f9342a454d8eb0d069625

syntax: fix hang parsing single quotes in parameter expansions
This regression was introduced in 5bb742d102, since we didn't include
enough test cases. In particular, one where the single quote directly
followed the forward slash token.

Fixes #626.

syntax/lexer.go
@@ -163,7 +163,7 @@ func (p *Parser) nextKeepSpaces() {
 		switch r {
 		case '}', '/':
 			p.tok = p.paramToken(r)
-		case '`', '"', '$':
+		case '`', '"', '$', '\'':
 			p.tok = p.regToken(r)
 		default:
 			p.advanceLitOther(r)

syntax/filetests_test.go
@@ -2074,6 +2074,16 @@ var fileTests = []testCase{
 		},
 	},
 	{
+		Strs: []string{`${var/'a'/b'c'd}`},
+		bsmk: &ParamExp{
+			Param: lit("var"),
+			Repl: &Replace{
+				Orig: word(sglQuoted("a")),
+				With: word(lit("b"), sglQuoted("c"), lit("d")),
+			},
+		},
+	},
+	{
 		Strs: []string{`${foo%bar}${foo%%bar*}`},
 		common: word(
 			&ParamExp{

syntax/parser_test.go
@@ -1715,6 +1715,10 @@ var shellTests = []errorCase{
 		bsmk: `1:6: reached EOF without matching ${ with }`,
 	},
 	{
+		in:   "echo ${a/''",
+		bsmk: `1:6: reached EOF without matching ${ with }`,
+	},
+	{
 		in:   "echo ${a-\n",
 		bsmk: `1:6: reached EOF without matching ${ with }`,
 	},