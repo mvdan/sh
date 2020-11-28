b8bd8e57547725220dbf237072b80e40c09a0502
Faustin Lammler <faustin@fala.red>
2020-11-28 11:48:12 +0100

    Build locally first, then push to DockerHub
    Based on the comment from @ArturKlauser
    (https://github.com/mvdan/sh/pull/638#discussion_r532089154), the author
    of the first buildx CI workflow, here is the workflow suggested for
    building and pushing docker images:
      - build against a local docker registry
      - test locally
      - push on DockerHub if tests passes
      - update the description on DockerHub
    
    Note: previous test was using a generated sh script, as we already have
    bash script in the repo (docker entry-point.sh), we decided to use it
    instead (and the script has been validate with shfmt).

--- cmd/shfmt/docker-entrypoint.sh
+++ cmd/shfmt/docker-entrypoint.sh
@@ -10,9 +10,9 @@
 set -eu
 
 # run command if it is not starting with a "-" and is an executable in PATH
-if [ "${#}" -gt 0 ] && \
-   [ "${1#-}" = "${1}" ] && \
-   command -v "${1}" > "/dev/null" 2>&1; then
+if [ "${#}" -gt 0 ] &&
+	[ "${1#-}" = "${1}" ] &&
+	command -v "${1}" >"/dev/null" 2>&1; then
 	exec "${@}"
 else
 	# else default to run the command
