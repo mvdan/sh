mvdan/sh

commit f4c774aa15046ef006508e182fde10c4b56876fa

interp: rewrite the scoping of variables
Before, we used the combination of Env with two maps, Vars and cmdVars,
to implement variable scopes. Vars was for regular variables like
"foo=bar", and cmdVars was for inline variables like "foo=bar baz".

This had a significant bug; inline variables can also be scoped, and
since we used a single map for them, an inner scope could modify an
outer scope:

	$ gosh -c 'f() { echo $inline; inline=bar true; echo $inline; }; inline=foo f'
	foo

	$ bash -c 'f() { echo $inline; inline=bar true; echo $inline; }; inline=foo f'
	foo
	foo

We could have fixed this by merging cmdVars into Vars, and fixing up the
restoration of previous values for inline variable assignments.

However, we also noticed that we implemented scoping in two different
ways: one via overlayEnviron, and another via making entire copies of
Vars and cmdVars.

Instead, internally track variables only via the Env field. Since it
must remain an expand.Environ for backwards compatibility, and an
interpreter must also modify variables, we add an unexported writeEnv
field too.

Now, all nested scopes are implemented via overlayEnviron, be it for
function calls, program executions, or any other reason.

Since Vars is an exported field, we can't get rid of it. It could have
been used to query what variables were set after running a program, so
to keep that working, we add a bit of code at the end of Run to fill the
map. Reset always emptied Vars, so it could have never be used to
pre-fill variables; that's what Env is for.

I've also left some v4 thoughts as TODOs. We might want to rethink the
WriteEnviron semantics, or even a larger interface supporting scopes, to
allow the user to control the entire environment behavior.

Fixes #668.

interp/vars.go
@@ -4,6 +4,7 @@
 package interp
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 	"strconv"
@@ -16,20 +17,60 @@ import (
 type overlayEnviron struct {
 	parent expand.Environ
 	values map[string]expand.Variable
+
+	// We need to know if the current scope is a function's scope, because
+	// functions can modify global variables.
+	funcScope bool
 }
 
-func (o overlayEnviron) Get(name string) expand.Variable {
+func (o *overlayEnviron) Get(name string) expand.Variable {
 	if vr, ok := o.values[name]; ok {
 		return vr
 	}
 	return o.parent.Get(name)
 }
 
-func (o overlayEnviron) Set(name string, vr expand.Variable) {
+func (o *overlayEnviron) Set(name string, vr expand.Variable) error {
+	// "foo=bar" in a function updates the global scope, unless the variable
+	// has been declared as local.
+	if o.funcScope && vr.IsSet() && !vr.Local && !o.values[name].Local {
+		return o.parent.(expand.WriteEnviron).Set(name, vr)
+	}
+
+	prev := o.Get(name)
+	if o.values == nil {
+		o.values = make(map[string]expand.Variable)
+	}
+	if !vr.IsSet() && (vr.Exported || vr.Local || vr.ReadOnly) {
+		// marking as exported/local/readonly
+		prev.Exported = prev.Exported || vr.Exported
+		prev.Local = prev.Local || vr.Local
+		prev.ReadOnly = prev.ReadOnly || vr.ReadOnly
+		vr = prev
+		o.values[name] = vr
+		return nil
+	}
+	if prev.ReadOnly {
+		return fmt.Errorf("readonly variable")
+	}
+	if !vr.IsSet() { // unsetting
+		if prev.Local {
+			vr.Local = true
+			o.values[name] = vr
+			return nil
+		}
+		delete(o.values, name)
+		if writeEnv, _ := o.parent.(expand.WriteEnviron); writeEnv != nil {
+			writeEnv.Set(name, vr)
+			return nil
+		}
+	}
+	// modifying the entire variable
 	o.values[name] = vr
+	return nil
 }
 
-func (o overlayEnviron) Each(f func(name string, vr expand.Variable) bool) {
+func (o *overlayEnviron) Each(f func(name string, vr expand.Variable) bool) {
 	o.parent.Each(f)
 	for name, vr := range o.values {
 		if !f(name, vr) {
@@ -53,7 +94,7 @@ func execEnv(env expand.Environ) []string {
 				}
 			}
 		}
-		if vr.Exported {
+		if vr.Exported && vr.Kind == expand.String {
 			list = append(list, name+"="+vr.String())
 		}
 		return true
@@ -104,22 +145,12 @@ func (r *Runner) lookupVar(name string) expand.Variable {
 	if vr.IsSet() {
 		return vr
 	}
-	if value, e := r.cmdVars[name]; e {
-		return expand.Variable{Kind: expand.String, Str: value}
-	}
-	if vr, e := r.funcVars[name]; e {
-		vr.Local = true
-		return vr
-	}
-	if vr, e := r.Vars[name]; e {
-		return vr
-	}
-	if vr := r.Env.Get(name); vr.IsSet() {
+	if vr := r.writeEnv.Get(name); vr.IsSet() {
 		return vr
 	}
 	if runtime.GOOS == "windows" {
 		upper := strings.ToUpper(name)
-		if vr := r.Env.Get(upper); vr.IsSet() {
+		if vr := r.writeEnv.Get(upper); vr.IsSet() {
 			return vr
 		}
 	}
@@ -131,17 +162,10 @@ func (r *Runner) envGet(name string) string {
 }
 
 func (r *Runner) delVar(name string) {
-	vr := r.lookupVar(name)
-	if vr.ReadOnly {
-		r.errf("%s: readonly variable\n", name)
+	if err := r.writeEnv.Set(name, expand.Variable{}); err != nil {
+		r.errf("%s: %v\n", name, err)
 		r.exit = 1
 		return
-	}
-	if vr.Local {
-		// don't overwrite a non-local var with the same name
-		r.funcVars[name] = expand.Variable{}
-	} else {
-		r.Vars[name] = expand.Variable{} // to not query r.Env
 	}
 }
 
@@ -150,31 +174,19 @@ func (r *Runner) setVarString(name, value string) {
 }
 
 func (r *Runner) setVarInternal(name string, vr expand.Variable) {
-	if vr.Kind == expand.String {
-		if r.opts[optAllExport] {
-			vr.Exported = true
-		}
-	} else {
-		vr.Exported = false
+	if r.opts[optAllExport] {
+		vr.Exported = true
 	}
-	if vr.Local {
-		if r.funcVars == nil {
-			r.funcVars = make(map[string]expand.Variable)
-		}
-		r.funcVars[name] = vr
-	} else {
-		r.Vars[name] = vr
+	if err := r.writeEnv.Set(name, vr); err != nil {
+		r.errf("%s: %v\n", name, err)
+		r.exit = 1
+		return
 	}
 }
 
 func (r *Runner) setVar(name string, index syntax.ArithmExpr, vr expand.Variable) {
 	cur := r.lookupVar(name)
-	if cur.ReadOnly {
-		r.errf("%s: readonly variable\n", name)
-		r.exit = 1
-		return
-	}
-	if name2, var2 := cur.Resolve(r.Env); name2 != "" {
+	if name2, var2 := cur.Resolve(r.writeEnv); name2 != "" {
 		name = name2
 		cur = var2
 	}
@@ -249,11 +261,10 @@ func stringIndex(index syntax.ArithmExpr) bool {
 	return false
 }
 
+// TODO: make assignVal and setVar consistent with the WriteEnviron interface
+
 func (r *Runner) assignVal(as *syntax.Assign, valType string) expand.Variable {
 	prev := r.lookupVar(as.Name.Value)
-	if as.Naked {
-		return prev
-	}
 	if as.Value != nil {
 		s := r.literal(as.Value)
 		if !as.Append || !prev.IsSet() {

interp/interp_test.go
@@ -30,7 +30,7 @@ import (
 // The timeout saves us from hangs or burning too much CPU if there are bugs.
 // All the test cases are designed to be inexpensive and stop in a very short
 // amount of time, so 5s should be plenty even for busy machines.
-const runnerRunTimeout = 5 * time.Second
+const runnerRunTimeout = 1 * time.Second
 
 // Some program which should be in $PATH. Needs to run before runTests is
 // initialized (so an init function wouldn't work), because runTest uses it.
@@ -361,6 +361,21 @@ var runTests = []runTest{
 	{`a=" x  y"; b=$a c="$a"; echo "$b"; echo "$c"`, " x  y\n x  y\n"},
 	// TODO: reenable once we figure out the broken pipe error
 	//{`$ENV_PROG | while read line; do if test -z "$line"; then echo empty; fi; break; done`, ""}, // never begin with an empty element
+
+	// inline variables have special scoping
+	{
+		"f() { echo $inline; inline=bar true; echo $inline; }; inline=foo f",
+		"foo\nfoo\n",
+	},
+	{"v=x; read v <<< 'y'; echo $v", "y\n"},
+	{"v=x; v=inline read v <<< 'y'; echo $v", "x\n"},
+	{"v=x; v=inline unset v; echo $v", "x\n"},
+	{"v=x; echo 'v=y' >f; v=inline source f; echo $v", "x\n"},
+	{"declare -n v=v2; v=inline true; echo $v $v2", "\n"},
+	{"f() { echo $v; }; v=x; v=y f; f", "y\nx\n"},
+	{"f() { echo $v; }; v=x; v+=y f; f", "xy\nx\n"},
+	{"f() { echo $v; }; declare -n v=v2; v2=x; v=y f; f", "y\nx\n"},
+	{"f() { echo ${v[@]}; }; v=(e1 e2); v=y f; f", "y\ne1 e2\n"},
 
 	// special vars
 	{"echo $?; false; echo $?", "0\n1\n"},
@@ -3572,8 +3587,7 @@ func TestMalformedPathOnWindows(t *testing.T) {
 }
 
 func TestReadShouldNotPanicWithNilStdin(t *testing.T) {
-	var b bytes.Buffer
-	r, err := New(StdIO(nil, &b, &b))
+	r, err := New()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -3583,5 +3597,23 @@ func TestReadShouldNotPanicWithNilStdin(t *testing.T) {
 	defer cancel()
 	if err := r.Run(ctx, f); err == nil {
 		t.Fatal("it should have retuned an error")
+	}
+}
+
+func TestRunnerVars(t *testing.T) {
+	r, err := New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f := parse(t, nil, "FOO=updated; BAR=new")
+	ctx, cancel := context.WithTimeout(context.Background(), runnerRunTimeout)
+	defer cancel()
+	if err := r.Run(ctx, f); err != nil {
+		t.Fatal(err)
+	}
+
+	if want, got := "updated", r.Vars["FOO"].String(); got != want {
+		t.Fatalf("wrong output:\nwant: %q\ngot:  %q", want, got)
 	}
 }

interp/api.go
@@ -38,9 +38,11 @@ import (
 // configured via runner options; once a Runner has been created, the fields
 // should be treated as read-only.
 type Runner struct {
-	// Env specifies the environment of the interpreter, which must be
-	// non-nil.
+	// Env specifies the initial environment for the interpreter, which must
+	// be non-nil.
 	Env expand.Environ
+
+	writeEnv expand.WriteEnviron
 
 	// Dir specifies the working directory of the command, which must be an
 	// absolute path.
@@ -51,7 +53,9 @@ type Runner struct {
 	Params []string
 
 	// Separate maps - note that bash allows a name to be both a var and a
-	// func simultaneously
+	// func simultaneously.
+	// Vars is mostly superseded by Env at this point.
+	// TODO(v4): remove these
 
 	Vars  map[string]expand.Variable
 	Funcs map[string]*syntax.Stmt
@@ -86,12 +90,6 @@ type Runner struct {
 	wgProcSubsts sync.WaitGroup
 
 	filename string // only if Node was a File
-
-	// like Vars, but local to a func i.e. "local foo=bar"
-	funcVars map[string]expand.Variable
-
-	// like Vars, but local to a cmd i.e. "foo=bar prog args..."
-	cmdVars map[string]string
 
 	// >0 to break or continue out of N enclosing loops
 	breakEnclosing, contnEnclosing int
@@ -418,7 +416,6 @@ func (r *Runner) Reset() {
 
 		// emptied below, to reuse the space
 		Vars:     r.Vars,
-		cmdVars:  r.cmdVars,
 		dirStack: r.dirStack[:0],
 		usedNew:  r.usedNew,
 	}
@@ -429,31 +426,28 @@ func (r *Runner) Reset() {
 			delete(r.Vars, k)
 		}
 	}
-	if r.cmdVars == nil {
-		r.cmdVars = make(map[string]string)
-	} else {
-		for k := range r.cmdVars {
-			delete(r.cmdVars, k)
-		}
-	}
-	if vr := r.Env.Get("HOME"); !vr.IsSet() {
+	// TODO(v4): Use the supplied Env directly if it implements enough methods.
+	r.writeEnv = &overlayEnviron{parent: r.Env}
+	if !r.writeEnv.Get("HOME").IsSet() {
 		home, _ := os.UserHomeDir()
-		r.Vars["HOME"] = expand.Variable{Kind: expand.String, Str: home}
+		r.setVarString("HOME", home)
 	}
-	r.Vars["UID"] = expand.Variable{
-		Kind:     expand.String,
-		ReadOnly: true,
-		Str:      strconv.Itoa(os.Getuid()),
+	if !r.writeEnv.Get("UID").IsSet() {
+		r.setVar("UID", nil, expand.Variable{
+			Kind:     expand.String,
+			ReadOnly: true,
+			Str:      strconv.Itoa(os.Getuid()),
+		})
 	}
-	r.Vars["PWD"] = expand.Variable{Kind: expand.String, Str: r.Dir}
-	r.Vars["IFS"] = expand.Variable{Kind: expand.String, Str: " \t\n"}
-	r.Vars["OPTIND"] = expand.Variable{Kind: expand.String, Str: "1"}
+	r.setVarString("PWD", r.Dir)
+	r.setVarString("IFS", " \t\n")
+	r.setVarString("OPTIND", "1")
 
 	if runtime.GOOS == "windows" {
 		// convert $PATH to a unix path list
-		path := r.Env.Get("PATH").String()
+		path := r.writeEnv.Get("PATH").String()
 		path = strings.Join(filepath.SplitList(path), ":")
-		r.Vars["PATH"] = expand.Variable{Kind: expand.String, Str: path}
+		r.setVarString("PATH", path)
 	}
 
 	r.dirStack = append(r.dirStack, r.Dir)
@@ -508,6 +502,12 @@ func (r *Runner) Run(ctx context.Context, node syntax.Node) error {
 	if r.exit != 0 {
 		r.setErr(NewExitStatus(uint8(r.exit)))
 	}
+	if r.Vars != nil {
+		r.writeEnv.Each(func(name string, vr expand.Variable) bool {
+			r.Vars[name] = vr
+			return true
+		})
+	}
 	return r.err
 }
 
@@ -534,7 +534,6 @@ func (r *Runner) Subshell() *Runner {
 	// Keep in sync with the Runner type. Manually copy fields, to not copy
 	// sensitive ones like errgroup.Group, and to do deep copies of slices.
 	r2 := &Runner{
-		Env:         r.Env,
 		Dir:         r.Dir,
 		Params:      r.Params,
 		execHandler: r.execHandler,
@@ -550,28 +549,28 @@ func (r *Runner) Subshell() *Runner {
 
 		origStdout: r.origStdout, // used for process substitutions
 	}
-	r2.Vars = make(map[string]expand.Variable, len(r.Vars))
-	for k, v := range r.Vars {
-		v2 := v
+	// Env vars and funcs are copied, since they might be modified.
+	// TODO(v4): lazy copying? it would probably be enough to add a
+	// copyOnWrite bool field to Variable, then a Modify method that must be
+	// used when one needs to modify a variable. ideally with some way to
+	// catch direct modifications without the use of Modify and panic,
+	// perhaps via a check when getting or setting vars at some level.
+	oenv := &overlayEnviron{parent: expand.ListEnviron()}
+	r.writeEnv.Each(func(name string, vr expand.Variable) bool {
+		vr2 := vr
 		// Make deeper copies of List and Map, but ensure that they remain nil
-		// if they are nil in v.
-		v2.List = append([]string(nil), v.List...)
-		if v.Map != nil {
-			v2.Map = make(map[string]string, len(v.Map))
-			for k, v := range v.Map {
-				v2.Map[k] = v
+		// if they are nil in vr.
+		vr2.List = append([]string(nil), vr.List...)
+		if vr.Map != nil {
+			vr2.Map = make(map[string]string, len(vr.Map))
+			for k, vr := range vr.Map {
+				vr2.Map[k] = vr
 			}
 		}
-		r2.Vars[k] = v2
-	}
-	r2.funcVars = make(map[string]expand.Variable, len(r.funcVars))
-	for k, v := range r.funcVars {
-		r2.funcVars[k] = v
-	}
-	r2.cmdVars = make(map[string]string, len(r.cmdVars))
-	for k, v := range r.cmdVars {
-		r2.cmdVars[k] = v
-	}
+		oenv.Set(name, vr2)
+		return true
+	})
+	r2.writeEnv = oenv
 	r2.Funcs = make(map[string]*syntax.Stmt, len(r.Funcs))
 	for k, v := range r.Funcs {
 		r2.Funcs[k] = v

interp/builtin.go
@@ -108,11 +108,9 @@ func (r *Runner) builtinCode(ctx context.Context, pos syntax.Pos, name string, a
 		}
 
 		for _, arg := range args {
-			if vr := r.lookupVar(arg); vr.IsSet() && vars {
+			if vars && r.lookupVar(arg).IsSet() {
 				r.delVar(arg)
-				continue
-			}
-			if _, ok := r.Funcs[arg]; ok && funcs {
+			} else if _, ok := r.Funcs[arg]; ok && funcs {
 				delete(r.Funcs, arg)
 			}
 		}
@@ -232,7 +230,7 @@ func (r *Runner) builtinCode(ctx context.Context, pos syntax.Pos, name string, a
 		args := fp.args()
 		for _, arg := range args {
 			if mode == "-p" {
-				if path, err := LookPathDir(r.Dir, expandEnv{r}, arg); err == nil {
+				if path, err := LookPathDir(r.Dir, r.writeEnv, arg); err == nil {
 					r.outf("%s\n", path)
 				} else {
 					anyNotFound = true
@@ -281,7 +279,7 @@ func (r *Runner) builtinCode(ctx context.Context, pos syntax.Pos, name string, a
 				}
 				continue
 			}
-			if path, err := LookPathDir(r.Dir, expandEnv{r}, arg); err == nil {
+			if path, err := LookPathDir(r.Dir, r.writeEnv, arg); err == nil {
 				if mode == "-t" {
 					r.out("file\n")
 				} else {
@@ -418,7 +416,7 @@ func (r *Runner) builtinCode(ctx context.Context, pos syntax.Pos, name string, a
 			last = 0
 			if r.Funcs[arg] != nil || isBuiltin(arg) {
 				r.outf("%s\n", arg)
-			} else if path, err := LookPathDir(r.Dir, expandEnv{r}, arg); err == nil {
+			} else if path, err := LookPathDir(r.Dir, r.writeEnv, arg); err == nil {
 				r.outf("%s\n", path)
 			} else {
 				last = 1
@@ -552,7 +550,7 @@ func (r *Runner) builtinCode(ctx context.Context, pos syntax.Pos, name string, a
 			if i < len(values) {
 				val = values[i]
 			}
-			r.setVar(name, nil, expand.Variable{Kind: expand.String, Str: val})
+			r.setVarString(name, val)
 		}
 
 		return 0
@@ -766,8 +764,8 @@ func (r *Runner) changeDir(path string) int {
 		return 1
 	}
 	r.Dir = path
-	r.Vars["OLDPWD"] = r.Vars["PWD"]
-	r.Vars["PWD"] = expand.Variable{Kind: expand.String, Str: path}
+	r.setVarString("OLDPWD", r.envGet("PWD"))
+	r.setVarString("PWD", path)
 	return 0
 }
 

interp/runner.go
@@ -171,7 +171,7 @@ func (r *Runner) pattern(word *syntax.Word) string {
 	return str
 }
 
-// expandEnv exposes Runner's variables to the expand package.
+// expandEnviron exposes Runner's variables to the expand package.
 type expandEnv struct {
 	r *Runner
 }
@@ -188,35 +188,17 @@ func (e expandEnv) Set(name string, vr expand.Variable) error {
 }
 
 func (e expandEnv) Each(fn func(name string, vr expand.Variable) bool) {
-	e.r.Env.Each(fn)
-	for name, vr := range e.r.Vars {
-		if !fn(name, vr) {
-			return
-		}
-	}
+	e.r.writeEnv.Each(fn)
 }
 
 func (r *Runner) handlerCtx(ctx context.Context) context.Context {
 	hc := HandlerContext{
+		Env:    &overlayEnviron{parent: r.writeEnv},
 		Dir:    r.Dir,
 		Stdin:  r.stdin,
 		Stdout: r.stdout,
 		Stderr: r.stderr,
 	}
-	oenv := overlayEnviron{
-		parent: r.Env,
-		values: make(map[string]expand.Variable),
-	}
-	for name, vr := range r.Vars {
-		oenv.Set(name, vr)
-	}
-	for name, vr := range r.funcVars {
-		oenv.Set(name, vr)
-	}
-	for name, value := range r.cmdVars {
-		oenv.Set(name, expand.Variable{Exported: true, Kind: expand.String, Str: value})
-	}
-	hc.Env = oenv
 	return context.WithValue(ctx, handlerCtxKey{}, hc)
 }
 
@@ -339,17 +321,28 @@ func (r *Runner) cmd(ctx context.Context, cm syntax.Command) {
 			}
 			break
 		}
+
+		type restoreVar struct {
+			name string
+			vr   expand.Variable
+		}
+		var restores []restoreVar
+
 		for _, as := range x.Assigns {
+			name := as.Name.Value
+			origVr := r.lookupVar(name)
+
 			vr := r.assignVal(as, "")
-			// we know that inline vars must be strings
-			r.cmdVars[as.Name.Value] = vr.Str
+			// Inline command vars are always exported.
+			vr.Exported = true
+
+			restores = append(restores, restoreVar{name, origVr})
+
+			r.setVarInternal(name, vr)
 		}
 		r.call(ctx, x.Args[0].Pos(), fields)
-		// cmdVars can be nuked here, as they are never useful
-		// again once we nest into further levels of inline
-		// vars.
-		for k := range r.cmdVars {
-			delete(r.cmdVars, k)
+		for _, restore := range restores {
+			r.setVarInternal(restore.name, restore.vr)
 		}
 	case *syntax.BinaryCmd:
 		switch x.Op {
@@ -516,7 +509,10 @@ func (r *Runner) cmd(ctx context.Context, cm syntax.Command) {
 					r.exit = 1
 					return
 				}
-				vr := r.assignVal(as, valType)
+				var vr expand.Variable
+				if !as.Naked {
+					vr = r.assignVal(as, valType)
+				}
 				if global {
 					vr.Local = false
 				} else if local {
@@ -531,7 +527,9 @@ func (r *Runner) cmd(ctx context.Context, cm syntax.Command) {
 					}
 				}
 				if as.Naked {
-					r.setVarInternal(name, vr)
+					if vr.Exported || vr.Local || vr.ReadOnly {
+						r.setVarInternal(name, vr)
+					}
 				} else {
 					r.setVar(name, as.Index, vr)
 				}
@@ -729,14 +727,18 @@ func (r *Runner) call(ctx context.Context, pos syntax.Pos, args []string) {
 		oldParams := r.Params
 		r.Params = args[1:]
 		oldInFunc := r.inFunc
-		oldFuncVars := r.funcVars
-		r.funcVars = nil
 		r.inFunc = true
+
+		// Functions run in a nested scope.
+		// Note that Runner.exec below does something similar.
+		origEnv := r.writeEnv
+		r.writeEnv = &overlayEnviron{parent: r.writeEnv, funcScope: true}
 
 		r.stmt(ctx, body)
 
+		r.writeEnv = origEnv
+
 		r.Params = oldParams
-		r.funcVars = oldFuncVars
 		r.inFunc = oldInFunc
 		if code, ok := r.err.(returnStatus); ok {
 			r.err = nil

commit 7cae848b3b8db8ab5c3e73e09e30694bed767c12

interp: local vars are mutable
Local variables should be mutable but f4c774a accidentally made them
immutable. A new assignment is made with a new `expand.Variable` struct,
which defaults the Local flag to false. Failing to preserve the
localness of an overlayed variable means future sets are incorrectly
forwarded to the global scope.

Fixes #674

interp/vars.go
@@ -66,6 +66,7 @@ func (o *overlayEnviron) Set(name string, vr expand.Variable) error {
 		}
 	}
 	// modifying the entire variable
+	vr.Local = prev.Local || vr.Local
 	o.values[name] = vr
 	return nil
 }

interp/interp_test.go
@@ -564,6 +564,10 @@ var runTests = []runTest{
 		`a=""; a+=(b); echo ${a[@]} ${#a[@]}`,
 		"b 2\n",
 	},
+	{
+		"f() { local a; a=bad; a=good; echo $a; }; f",
+		"good\n",
+	},
 
 	// if
 	{