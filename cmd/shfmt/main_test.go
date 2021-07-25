d4d3c6fed0c82a1c0330a51f39fc28d124eb7039
Daniel Martí <mvdan@mvdan.cc>
2021-07-25 23:32:13 +0100

    bump testscript version
    Note that this required a minor change to adapt to v1.8.0; now,
    top-level commands are set up in $PATH by default, and
    TESTSCRIPT_COMMAND is no longer obeyed.
    
    Our life is easier, really. We can now simply rely on our shfmt being
    the first match in $PATH. I verified that this really works by
    uninstalling all shfmt binaries from my original $PATH.

--- cmd/shfmt/main_test.go
+++ cmd/shfmt/main_test.go
@@ -5,10 +5,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
@@ -25,23 +23,7 @@ var update = flag.Bool("u", false, "update testscript output files")
 func TestScripts(t *testing.T) {
 	t.Parallel()
 	testscript.Run(t, testscript.Params{
-		Dir: filepath.Join("testdata", "scripts"),
-		Setup: func(env *testscript.Env) error {
-			bindir := filepath.Join(env.WorkDir, ".bin")
-			if err := os.Mkdir(bindir, 0o777); err != nil {
-				return err
-			}
-			binfile := filepath.Join(bindir, "bundled-shfmt")
-			if runtime.GOOS == "windows" {
-				binfile += ".exe"
-			}
-			if err := os.Symlink(os.Args[0], binfile); err != nil {
-				return err
-			}
-			env.Vars = append(env.Vars, fmt.Sprintf("PATH=%s%c%s", bindir, filepath.ListSeparator, os.Getenv("PATH")))
-			env.Vars = append(env.Vars, "TESTSCRIPT_COMMAND=shfmt")
-			return nil
-		},
+		Dir:           filepath.Join("testdata", "scripts"),
 		UpdateScripts: *update,
 	})
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/kr/pretty v0.2.1
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pkg/diff v0.0.0-20210226163009-20ebb0f2a09e
-	github.com/rogpeppe/go-internal v1.7.0
+	github.com/rogpeppe/go-internal v1.8.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/sys v0.0.0-20210503080704-8803ae5d1324
 	golang.org/x/term v0.0.0-20210503060354-a79de5458b56
