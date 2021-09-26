e186e04cbf119ac6e51d2010cb37674374557046
Daniel Martí <mvdan@mvdan.cc>
2021-09-26 22:25:51 +0100

    syntax: convert the parser+printer fuzzer to testing.F
    We already use Go 1.18's fuzzing support for FuzzQuote,
    and since it's working well, do it here too.
    
    It supports structured input parameters,
    which will mean being able to get rid of the bit math.
    For now, just split the []byte into three components,
    and add a TODO for splitting those up further.
    
    Finally, we add a TODO about the initial corpus.
    With go-fuzz, we relied on a stored corpus in a git branch.
    With testing.F, we'll need something slightly different.

--- syntax/fuzz_test.go
+++ syntax/fuzz_test.go
@@ -1,10 +1,15 @@
+// Copyright (c) 2016, Daniel Martí <mvdan@mvdan.cc>
+// See LICENSE for licensing information
+
 //go:build go1.18
 // +build go1.18
 
 package syntax_test
 
 import (
+	"io/ioutil"
 	"os/exec"
+	"strings"
 	"testing"
 
 	"mvdan.cc/sh/v3/syntax"
@@ -43,6 +48,77 @@ func FuzzQuote(f *testing.F) {
 		want, got := s, string(out)
 		if want != got {
 			t.Fatalf("output mismatch on %q quoted as %s: got %q (len=%d)", want, quoted, got, len(got))
+		}
+	})
+}
+
+func FuzzParsePrint(f *testing.F) {
+	// TODO: turn these into separate option parameters
+	var zeroParserOpts uint8
+	var zeroPrinterOpts uint16
+
+	// TODO: probably use f.Add on table-driven test cases too?
+	// In the past, any crashers found by go-fuzz got put there.
+
+	f.Add("echo foo", zeroParserOpts, zeroPrinterOpts)
+	f.Add("'foo' \"bar\" $'baz'", zeroParserOpts, zeroPrinterOpts)
+	f.Add("if foo; then bar; baz; fi", zeroParserOpts, zeroPrinterOpts)
+	f.Add("{ (foo; bar); baz; }", zeroParserOpts, zeroPrinterOpts)
+	f.Add("$foo ${bar} ${baz[@]} $", zeroParserOpts, zeroPrinterOpts)
+	f.Add("foo >bar <<EOF\nbaz\nEOF", zeroParserOpts, zeroPrinterOpts)
+	f.Add("foo=bar baz=(x y z)", zeroParserOpts, zeroPrinterOpts)
+	f.Add("foo && bar || baz", zeroParserOpts, zeroPrinterOpts)
+	f.Add("foo | bar # baz", zeroParserOpts, zeroPrinterOpts)
+	f.Add("foo \\\n bar \\\\ba\\z", zeroParserOpts, zeroPrinterOpts)
+
+	f.Fuzz(func(t *testing.T, src string, parserOpts uint8, printerOpts uint16) {
+		// Below are the bit masks for parserOpts and printerOpts.
+		// Most masks are a single bit, for boolean options.
+		const (
+			// parser
+			// TODO: also fuzz StopAt
+			maskLangVariant  = 0b0000_0011 // two bits; 0-3 matching the iota
+			maskKeepComments = 0b0000_0100
+			maskSimplify     = 0b0000_1000 // pretend it's a parser option
+
+			// printer
+			maskIndent           = 0b0000_0000_0000_1111 // three bits; 0-15
+			maskBinaryNextLine   = 0b0000_0000_0001_0000
+			maskSwitchCaseIndent = 0b0000_0000_0010_0000
+			maskSpaceRedirects   = 0b0000_0000_0100_0000
+			maskKeepPadding      = 0b0000_0000_1000_0000
+			maskMinify           = 0b0000_0001_0000_0000
+			maskSingleLine       = 0b0000_0010_0000_0000
+			maskFunctionNextLine = 0b0000_0100_0000_0000
+		)
+
+		parser := syntax.NewParser()
+		lang := syntax.LangVariant(parserOpts & maskLangVariant) // range 0-3
+		syntax.Variant(lang)(parser)
+		syntax.KeepComments(parserOpts&maskKeepComments != 0)(parser)
+
+		prog, err := parser.Parse(strings.NewReader(src), "")
+		if err != nil {
+			t.Skip() // not valid shell syntax
+		}
+
+		if parserOpts&maskSimplify != 0 {
+			syntax.Simplify(prog)
+		}
+
+		printer := syntax.NewPrinter()
+		indent := uint(printerOpts & maskIndent) // range 0-15
+		syntax.Indent(indent)(printer)
+		syntax.BinaryNextLine(printerOpts&maskBinaryNextLine != 0)(printer)
+		syntax.SwitchCaseIndent(printerOpts&maskSwitchCaseIndent != 0)(printer)
+		syntax.SpaceRedirects(printerOpts&maskSpaceRedirects != 0)(printer)
+		syntax.KeepPadding(printerOpts&maskKeepPadding != 0)(printer)
+		syntax.Minify(printerOpts&maskMinify != 0)(printer)
+		syntax.SingleLine(printerOpts&maskSingleLine != 0)(printer)
+		syntax.FunctionNextLine(printerOpts&maskFunctionNextLine != 0)(printer)
+
+		if err := printer.Print(ioutil.Discard, prog); err != nil {
+			t.Skip() // e.g. invalid option
 		}
 	})
 }
