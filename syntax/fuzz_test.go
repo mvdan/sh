6954675667051f16097eb202b9381c863f83b2b3
Daniel Martí <mvdan@mvdan.cc>
2021-09-28 15:06:19 +0100

    syntax: make FuzzParsePrint take structured parameters
    No more messing with bit masks.
    The performance of the fuzzer seems largely the same,
    though it does seem to discover interesting inputs much quicker.
    
    That makes sense; it's now more aware of which knobs matter.
    
    We also move the fuzzers to be in the same syntax test package,
    so they can see all the other test data.

--- syntax/fuzz_test.go
+++ syntax/fuzz_test.go
@@ -4,15 +4,13 @@
 //go:build go1.18
 // +build go1.18
 
-package syntax_test
+package syntax
 
 import (
 	"io"
 	"os/exec"
 	"strings"
 	"testing"
-
-	"mvdan.cc/sh/v3/syntax"
 )
 
 func FuzzQuote(f *testing.F) {
@@ -31,7 +29,7 @@ func FuzzQuote(f *testing.F) {
 	f.Add("invalid-\xe2'")
 	f.Add("nonprint-\x0b\x1b")
 	f.Fuzz(func(t *testing.T, s string) {
-		quoted, ok := syntax.Quote(s)
+		quoted, ok := Quote(s)
 		if !ok {
 			// Contains a null byte; not interesting.
 			t.Skip()
@@ -57,69 +55,75 @@ func FuzzQuote(f *testing.F) {
 }
 
 func FuzzParsePrint(f *testing.F) {
-	// TODO: turn these into separate option parameters
-	var zeroParserOpts uint8
-	var zeroPrinterOpts uint16
-
 	// TODO: probably use f.Add on table-driven test cases too?
 	// In the past, any crashers found by go-fuzz got put there.
 
-	f.Add("echo foo", zeroParserOpts, zeroPrinterOpts)
-	f.Add("'foo' \"bar\" $'baz'", zeroParserOpts, zeroPrinterOpts)
-	f.Add("if foo; then bar; baz; fi", zeroParserOpts, zeroPrinterOpts)
-	f.Add("{ (foo; bar); baz; }", zeroParserOpts, zeroPrinterOpts)
-	f.Add("$foo ${bar} ${baz[@]} $", zeroParserOpts, zeroPrinterOpts)
-	f.Add("foo >bar <<EOF\nbaz\nEOF", zeroParserOpts, zeroPrinterOpts)
-	f.Add("foo=bar baz=(x y z)", zeroParserOpts, zeroPrinterOpts)
-	f.Add("foo && bar || baz", zeroParserOpts, zeroPrinterOpts)
-	f.Add("foo | bar # baz", zeroParserOpts, zeroPrinterOpts)
-	f.Add("foo \\\n bar \\\\ba\\z", zeroParserOpts, zeroPrinterOpts)
+	add := func(src string) {
+		// For now, default to just LangBash with KeepComments.
+		f.Add(src, uint8(LangBash), true, false,
+			uint8(0), false, false, false, false, false, false, false)
+	}
 
-	f.Fuzz(func(t *testing.T, src string, parserOpts uint8, printerOpts uint16) {
-		// Below are the bit masks for parserOpts and printerOpts.
-		// Most masks are a single bit, for boolean options.
-		const (
-			// parser
-			// TODO: also fuzz StopAt
-			maskLangVariant  = 0b0000_0011 // two bits; 0-3 matching the iota
-			maskKeepComments = 0b0000_0100
-			maskSimplify     = 0b0000_1000 // pretend it's a parser option
+	add("echo foo")
+	add("'foo' \"bar\" $'baz'")
+	add("if foo; then bar; baz; fi")
+	add("{ (foo; bar); baz; }")
+	add("$foo ${bar} ${baz[@]} $")
+	add("foo >bar <<EOF\nbaz\nEOF")
+	add("foo=bar baz=(x y z)")
+	add("foo && bar || baz")
+	add("foo | bar # baz")
+	add("foo \\\n bar \\\\ba\\z")
 
-			// printer
-			maskIndent           = 0b0000_0000_0000_1111 // three bits; 0-15
-			maskBinaryNextLine   = 0b0000_0000_0001_0000
-			maskSwitchCaseIndent = 0b0000_0000_0010_0000
-			maskSpaceRedirects   = 0b0000_0000_0100_0000
-			maskKeepPadding      = 0b0000_0000_1000_0000
-			maskMinify           = 0b0000_0001_0000_0000
-			maskSingleLine       = 0b0000_0010_0000_0000
-			maskFunctionNextLine = 0b0000_0100_0000_0000
-		)
+	f.Fuzz(func(t *testing.T,
+		src string,
 
-		parser := syntax.NewParser()
-		lang := syntax.LangVariant(parserOpts & maskLangVariant) // range 0-3
-		syntax.Variant(lang)(parser)
-		syntax.KeepComments(parserOpts&maskKeepComments != 0)(parser)
+		// parser options
+		// TODO: also fuzz StopAt
+		langVariant uint8, // 0-3
+		keepComments bool,
+
+		simplify bool,
+
+		// printer options
+		indent uint8, // 0-255
+		binaryNextLine bool,
+		switchCaseIndent bool,
+		spaceRedirects bool,
+		keepPadding bool,
+		minify bool,
+		singleLine bool,
+		functionNextLine bool,
+	) {
+		if langVariant > 3 {
+			t.Skip() // lang variants are 0-3
+		}
+		if indent > 16 {
+			t.Skip() // more indentation won't really be interesting
+		}
+
+		parser := NewParser()
+		Variant(LangVariant(langVariant))(parser)
+		KeepComments(keepComments)(parser)
 
 		prog, err := parser.Parse(strings.NewReader(src), "")
 		if err != nil {
 			t.Skip() // not valid shell syntax
 		}
 
-		if parserOpts&maskSimplify != 0 {
-			syntax.Simplify(prog)
+		if simplify {
+			Simplify(prog)
 		}
 
-		printer := syntax.NewPrinter()
-		indent := uint(printerOpts & maskIndent) // range 0-15
-		syntax.Indent(indent)(printer)
-		syntax.BinaryNextLine(printerOpts&maskBinaryNextLine != 0)(printer)
-		syntax.SwitchCaseIndent(printerOpts&maskSwitchCaseIndent != 0)(printer)
-		syntax.SpaceRedirects(printerOpts&maskSpaceRedirects != 0)(printer)
-		syntax.KeepPadding(printerOpts&maskKeepPadding != 0)(printer)
-		syntax.Minify(printerOpts&maskMinify != 0)(printer)
-		syntax.SingleLine(printerOpts&maskSingleLine != 0)(printer)
-		syntax.FunctionNextLine(printerOpts&maskFunctionNextLine != 0)(printer)
+		printer := NewPrinter()
+		Indent(uint(indent))(printer)
+		BinaryNextLine(binaryNextLine)(printer)
+		SwitchCaseIndent(switchCaseIndent)(printer)
+		SpaceRedirects(spaceRedirects)(printer)
+		KeepPadding(keepPadding)(printer)
+		Minify(minify)(printer)
+		SingleLine(singleLine)(printer)
+		FunctionNextLine(functionNextLine)(printer)
 
 		if err := printer.Print(io.Discard, prog); err != nil {
 			t.Skip() // e.g. invalid option
