b3d96ede031d996cf5f2004549a0c1804a4556f9
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2024-12-02 22:13:45 +0100

    interp: improve method signature, and add godoc links
    this is a zero code changes. The signature was already the right one.
    We are just using an alias, that will make things clearer.

--- interp/builtin.go
+++ interp/builtin.go
@@ -902,9 +902,9 @@ func (r *Runner) builtinCode(ctx context.Context, pos syntax.Pos, name string, a
 	return 0
 }
 
-// mapfileSplit returns a suitable Split function for a [bufio.Scanner];
+// mapfileSplit returns a function that satisfies [bufio.SplitFunc] interface, [bufio.Scanner] will use it to split;
 // the code is mostly stolen from [bufio.ScanLines].
-func mapfileSplit(delim byte, dropDelim bool) func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+func mapfileSplit(delim byte, dropDelim bool) bufio.SplitFunc {
 	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 		if atEOF && len(data) == 0 {
 			return 0, nil, nil
