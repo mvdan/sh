63544658a831b8e6bd4ab84ee4ef555e786133a0
Amjad Hossain <amjad.jibon@gmail.com>
2026-04-06 21:49:37 +0800

    refactor: introduce configurable AccessHandler to decouple file permission checks from the Runner

--- interp/os_notunix.go
+++ interp/os_notunix.go
@@ -8,6 +8,7 @@ package interp
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"mvdan.cc/sh/v3/syntax"
 )
@@ -16,30 +17,31 @@ func mkfifo(path string, mode uint32) error {
 	return fmt.Errorf("unsupported")
 }
 
-// access attempts to emulate [unix.Access] on Windows.
-// Windows seems to have a different system of permissions than Unix,
-// so for now just rely on what [io/fs.FileInfo] gives us.
-func (r *Runner) access(ctx context.Context, path string, mode uint32) error {
-	info, err := r.lstat(ctx, path)
-	if err != nil {
-		return err
+// DefaultAccessHandler returns the [AccessHandlerFunc] used by default on non-Unix systems.
+// It checks permission bits from file mode.
+func DefaultAccessHandler() AccessHandlerFunc {
+	return func(ctx context.Context, path string, mode uint32) error {
+		info, err := os.Stat(path)
+		if err != nil {
+			return err
+		}
+		m := info.Mode()
+		switch mode {
+		case access_R_OK:
+			if m&0o400 == 0 {
+				return fmt.Errorf("file is not readable")
+			}
+		case access_W_OK:
+			if m&0o200 == 0 {
+				return fmt.Errorf("file is not writable")
+			}
+		case access_X_OK:
+			if m&0o100 == 0 {
+				return fmt.Errorf("file is not executable")
+			}
+		}
+		return nil
 	}
-	m := info.Mode()
-	switch mode {
-	case access_R_OK:
-		if m&0o400 == 0 {
-			return fmt.Errorf("file is not readable")
-		}
-	case access_W_OK:
-		if m&0o200 == 0 {
-			return fmt.Errorf("file is not writable")
-		}
-	case access_X_OK:
-		if m&0o100 == 0 {
-			return fmt.Errorf("file is not executable")
-		}
-	}
-	return nil
 }
 
 // unTestOwnOrGrp panics. Under Unix, it implements the -O and -G unary tests,
