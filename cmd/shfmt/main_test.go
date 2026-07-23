0175cdea538ee95332ad22fae1cfe2c0838f1146
Daniel Martí <mvdan@mvdan.cc>
2026-07-23 21:16:08 +0200

    cmd/shfmt: add test case for shebang files without a final newline

--- cmd/shfmt/main_test.go
+++ cmd/shfmt/main_test.go
@@ -4,7 +4,9 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -25,5 +27,24 @@ func TestScript(t *testing.T) {
 		Dir:                 filepath.Join("testdata", "script"),
 		UpdateScripts:       *update,
 		RequireExplicitExec: true,
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			// txtar archive files always end with a newline,
+			// so this helps test files which do not end with one.
+			"trim-newline": func(ts *testscript.TestScript, neg bool, args []string) {
+				if neg || len(args) == 0 {
+					ts.Fatalf("usage: trim-newline file...")
+				}
+				for _, arg := range args {
+					path := ts.MkAbs(arg)
+					data, err := os.ReadFile(path)
+					ts.Check(err)
+					trimmed, ok := bytes.CutSuffix(data, []byte("\n"))
+					if !ok {
+						ts.Fatalf("%s does not end with a newline", arg)
+					}
+					ts.Check(os.WriteFile(path, trimmed, 0o666))
+				}
+			},
+		},
 	})
 }
